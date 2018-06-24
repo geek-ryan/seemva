51398175cddf57f0baa3f26135906f7a33194b8e
chiabi <chiabi88@gmail.com>
2018-06-25 00:50:01 +0900

    Modify SingUpFormPC Add checkbox to change input type

--- src/components/SignUpFormPC.js
+++ src/components/SignUpFormPC.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Form, Input, Icon, Button } from 'antd';
+import { Form, Input, Icon, Button, Checkbox } from 'antd';
 
 const FormItem = Form.Item;
 
 class SingUpPC extends Component {
   render() {
+    const { onChange, checked } = this.props;
     return (
       <div>
         <h2>Creat New Account</h2>
@@ -12,7 +13,16 @@ class SingUpPC extends Component {
           <FormItem>
             <Input prefix={<Icon type="mail" />} placeholder="email" />
             <Input prefix={<Icon type="user" />} placeholder="username" />
-            <Input prefix={<Icon type="lock" />} placeholder="password" />
+            <span>
+              view password <Checkbox onChange={onChange} />
+            </span>
+            <Input
+              type={checked ? 'text' : 'password'}
+              prefix={<Icon type="lock" />}
+              placeholder="password"
+            />
+          </FormItem>
+          <FormItem>
             <Button
               type="primary"
               htmlType="submit"
