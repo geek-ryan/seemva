f102fbf52dfde355ea5c6b65d4f8c25bcad310fd
akira <akiraeiphoto@gmail.com>
2018-06-24 20:49:20 +0900

    Add SidebarOpenButton

---
+++ src/components/SidebarOpenButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Form, Input, Icon, Button } from 'antd';
+
+class SidebarOpenButton extends Component {
+  render() {
+    return <Icon type="right-circle-o" />;
+  }
+}
+
+export default SidebarOpenButton;
