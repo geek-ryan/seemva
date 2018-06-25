272812fc02095b21fb56c9715604b72a9919502d
chiabi <chiabi88@gmail.com>
2018-06-26 02:04:29 +0900

    Fix issue : antd getFieldDecorator in storybook 'getFieldDecorator is not function' bug fix Add hoc(Form.create) and Class component that has a Form component to solve

--- src/components/PasswordTogglePC.stories.js
+++ src/components/PasswordTogglePC.stories.js
@@ -9,6 +9,19 @@ import { addDecorator } from '@storybook/react/dist/client/preview';
 import { Form } from 'antd';
 import PasswordTogglePC from './PasswordTogglePC';
 
+class FormComponent extends React.Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form>
+        <PasswordTogglePC getFieldDecorator={getFieldDecorator} />
+      </Form>
+    );
+  }
+}
+
+const WrappedComponent = Form.create()(FormComponent);
+
 storiesOf('Password toggle PC', module).add('empty', () => (
-  <PasswordTogglePC />
+  <WrappedComponent />
 ));
