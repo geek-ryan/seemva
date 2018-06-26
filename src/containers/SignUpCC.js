80416939c22a700ceddce8b701d0df2b040d9ac8
chiabi <chiabi88@gmail.com>
2018-06-26 22:24:42 +0900

    Fix bug : onSubmitRegister function Edit typo : onSubmitLogin -> onSubmitResiter Change immediate test with unresolved errors to Verification on submit

--- src/containers/SignUpCC.js
+++ src/containers/SignUpCC.js
@@ -27,7 +27,6 @@ class SignUpCC extends Component {
                     loading={loading}
                     onOpenDialog={openUploadWiget}
                     onDeletProfile={async () => {
-                      console.log('deleleelelel');
                       await deleteProfile();
                     }}
                   />
@@ -45,9 +44,6 @@ class SignUpCC extends Component {
                         }
                       }
                     }}
-                    onBlurUserName={username =>
-                      users.some(user => user.username === username)
-                    }
                     errorCode={errorCode}
                   />
                 </React.Fragment>
