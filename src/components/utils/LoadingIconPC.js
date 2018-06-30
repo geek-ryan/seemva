b15cbcdcc1c488452c2ed78ac2c6a50052000abe
akiraei <akiraeiphoto@gmail.com>
2018-06-30 20:18:21 +0900

    Add loading PC

---
+++ src/components/utils/LoadingIconPC.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Icon } from 'antd';
+
+class LoadingIconPC extends Component {
+  render() {
+    return <Icon style={{ height: '100%', width: '100%' }} type="loading" />;
+  }
+}
+
+export default LoadingIconPC;
